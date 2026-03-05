1b921a22da43f015651c270e63b24139ca71964e
Munish0204 <munishwaran933@gmail.com>
2026-03-05 14:31:03 +0530

    Creating Poll is Completed

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/petitions", require("./routes/petitionRoutes"));
+app.use("/api/polls", require("./routes/pollRoutes"));
 
 app.get("/", (req, res) => {
   res.send("Civix Backend Running...");
